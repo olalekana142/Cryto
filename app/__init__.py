fb451c86c974d24f583ba9356830315ddbd213df
Olalekan <olalekana142@gmail.com>
2025-03-04 10:56:34 -0500

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app.routes import main
+app.register_blueprint(main)
+
 def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    from app.routes import main
-    app.register_blueprint(main)
-
     return app
